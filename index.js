f76f7e73fe2aee65aee7e6a915213107645f7928
Abdulateef Al-radaee <abdulateef.radaee@gmail.com>
2020-08-09 13:22:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(name, email) {
+  let url = 'http://localhost:3000/users';
+  let data = {
+    name,
+    email
+  }
+  let configObj = {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(data)
+  };
+
+  return fetch(url, configObj)
+  .then(function(response){
+    return response.json();
+  })
+  .then(function(json){
+    document.body.innerHTML = json['id'];
+  }).catch(function(error){
+    document.body.innerHTML = error.message;
+  });
+}
