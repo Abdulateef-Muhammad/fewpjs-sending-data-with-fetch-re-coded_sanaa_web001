c7e993c41d253418c29d1e34aa31b4e4b2491f5f
Abdulateef Al-radaee <abdulateef.radaee@gmail.com>
2020-08-09 13:16:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,59 +1,59 @@
-// // Add your code here
-// submitData('john Doe', 'johndoe@example.com')
-// .then(function(response){
-//   return response.json();
-// })
-// .then(function(json){
-//   let infoBox = document.createElement('div');
-//   infoBox.setAttribute('id', 'info-box');
-//   document.body.appendChild(infoBox);
-//   // infoBox.innerHTML += '<p>' + json.name + '</p>';
-//   // infoBox.innerHTML += '<p>' + json.email + '</p>';
-//   infoBox.innerHTML = json.id;
-//
-// }).catch(function(error){
-//   document.body.innerHTML = error.message;
-// });
-//
-//
-//
-// function submitData(name, email) {
-//   let url = 'http://localhost:3000/users';
-//   let data = {
-//     "name": name,
-//     "email": email
-//   }
-//   let configObj = {
-//     method: 'POST',
-//     headers: {
-//       "Content-Type": "application/json",
-//       "Accept": "application/json"
-//     },
-//     body: JSON.stringify(data)
-//   };
-//
-//   return fetch(url, configObj);
-// }
+// Add your code here
+submitData('john Doe', 'johndoe@example.com')
+.then(function(response){
+  return response.json();
+})
+.then(function(json){
+  let infoBox = document.createElement('div');
+  infoBox.setAttribute('id', 'info-box');
+  document.body.appendChild(infoBox);
+  // infoBox.innerHTML += '<p>' + json.name + '</p>';
+  // infoBox.innerHTML += '<p>' + json.email + '</p>';
+  infoBox.innerHTML = json.id;
 
-function submitData( name, email ) {
-  return fetch( 'http://localhost:3000/users', {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-      },
-      body: JSON.stringify( {
-        name,
-        email
-      } )
-    } )
-    .then( function ( response ) {
-      return response.json()
-    } )
-    .then( function ( object ) {
-      document.body.innerHTML = object[ "id" ]
-    } )
-    .catch( function ( error ) {
-      document.body.innerHTML = error.message
-    } )
+}).catch(function(error){
+  document.body.innerHTML = error.message;
+});
+
+
+
+function submitData(name, email) {
+  let url = 'http://localhost:3000/users';
+  let data = {
+    name,
+    email
+  }
+  let configObj = {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(data)
+  };
+
+  return fetch(url, configObj);
 }
+
+// function submitData( name, email ) {
+//   return fetch( 'http://localhost:3000/users', {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json"
+//       },
+//       body: JSON.stringify( {
+//         name,
+//         email
+//       } )
+//     } )
+//     .then( function ( response ) {
+//       return response.json()
+//     } )
+//     .then( function ( object ) {
+//       document.body.innerHTML = object[ "id" ]
+//     } )
+//     .catch( function ( error ) {
+//       document.body.innerHTML = error.message
+//     } )
+// }
